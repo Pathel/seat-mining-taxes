0e20927db07b627a13b38f5b21fabbb45954978d
Dustin <QuarQuar@gmail.com>
2018-12-10 02:53:24 -0500

    Add service provider for package bootstrapping.

---
+++ src/MiningTaxesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pathel\Seat\MoonMiningTaxes;
+
+use Illuminate\Support\ServiceProvider;
+
+class MiningTaxesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
