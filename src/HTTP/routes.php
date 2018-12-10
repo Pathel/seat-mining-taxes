936de1801c83adfb7e50fc8835b0caab8c891765
Dustin <QuarQuar@gmail.com>
2018-12-10 03:00:51 -0500

    Add routes.php skeleton.

---
+++ src/HTTP/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// Namespace all of the routes for this package.
+Route::group([
+    'namespace'  => 'Pathel\Seat\MoonMiningTaxes\Http\Controllers',
+    'middleware' => 'web',   // Web middleware for state etc since L5.3
+], function () {
+
+    // TODO: custom routes go here.  See routes.php in the web package for an example.
+
+});
